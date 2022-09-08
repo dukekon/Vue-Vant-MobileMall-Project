23b01512a7d9faef4ab7c95e1ba1c89d43b65597
duke <19833038@qq.com>
2022-09-08 18:23:02 +0800

    install plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,36 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Component from 'unplugin-vue-components/vite'
+import {VantResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Component({
+      resolvers: [VantResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        require('postcss-pxtorem')({
+          rootValue: 100,
+          propList: ['*'],
+          unitPrecision: 6,
+          replace: true,
+          minPixelValue: 0
+        })
+      ]
+    }
+  },
+  server: {
+    host: '0.0.0.0'
   }
 })
