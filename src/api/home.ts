63cb002f570d8ab82beeaf48dd71719fda9b4eef
duke <19833038@qq.com>
2022-10-05 00:52:26 +0800

    添加路由守卫

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,11 +1,3 @@
 import request from '@/utils/request'
 
 export const getHomeData = () => request.get('index/index')
-
-export type RootObject = RootObjectChild[];
-
-export interface RootObjectChild {
-  user: string;
-  text: string;
-  date: string;
-}
